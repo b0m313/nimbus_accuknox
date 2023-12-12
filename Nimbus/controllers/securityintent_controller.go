821385c3b2b0c44c6af9a86d31a8a2377236e878
b0m313 <13.spring.03@gmail.com>
2023-12-12 18:07:20 +0000

    Update: securityintentbinding CRD

--- Nimbus/controllers/securityintent_controller.go
+++ Nimbus/controllers/securityintent_controller.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"git.cclab-inu.com/b0m313/nimbus/operator/api/v1"
+	"github.com/5GSEC/nimbus/Nimbus/api/v1"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	general "github.com/5GSEC/nimbus/Nimbus/controllers/general"
